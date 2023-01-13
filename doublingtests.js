7f9f24e14f355ae8d3cccce180a24c4daf7fb3fc
dmcmahon71798 <98066481+dmcmahon71798@users.noreply.github.com>
2023-01-13 17:11:42 -0600

    -added egg collectibles -added condition for winning the game -added background for victory screen

--- doublingtests.js
+++ doublingtests.js
@@ -32,14 +32,46 @@ Crafty.init();
             this.xposition = xposition;
             this.yposition = yposition;
             this.graphic = Crafty.e("2D, Canvas, Color, Fourway").attr({x:this.xposition, y:this.yposition, w:this.width, h:this.height}).color(this.color).fourway(3);
+            if(entityName === "Egg")
+            {
+                this.graphic.color("white");
+                this.graphic.attr({w:50,h:50,z:1});
+            }
         }
   }
     const players = [];
     const nest = [];
-    const trees = [];
-    players.push(new Player("Jackdaw",50,50,5));
-    trees.push(new Tree("Tree",100,100));
-    trees.push(new Tree("Tree",500,500));
+    let trees = [];
+    let required = 0;
+    let score = 0;
+    document.body.style.backgroundColor = "chartreuse";
+    players.push(new Player("Jackdaw",Math.random() * 2000,Math.random() * 1350,5));
+    for(let i = 0; i < 100; i++)
+    {
+        trees.push(new Tree("Tree",Math.random() * 2000,Math.random() * 1350));
+    }
+    for(let i = 0; i < 1; i++)
+    {
+        let x = Math.random() * 2000;
+            let y = Math.random() * 1350;
+        while(
+            trees.find((value,index,obj) => {
+                if(x < value.graphic.x + value.graphic.w && 
+                x + 50 > value.graphic.x &&
+                y < value.graphic.y + value.graphic.h &&
+                50 + y > value.graphic.y) {
+                    return true;
+                }
+                return false;
+            })
+        )
+        {
+            x = Math.random() * 2000;
+            y = Math.random() * 1350;
+        }
+        trees.push(new Tree("Egg",x,y));
+        required++;
+    }
     const player = players.find((value,index,obj) => {
         return value.playerName === "Jackdaw";
     });
@@ -151,6 +183,19 @@ setInterval(() => {
                 collisionDirections.down = false;
                 collisionDirections.left = false;
             }
+            if(collide.entityName === "Egg")
+            {
+                score++;
+                console.log(`Score: ${score}/${required}`);
+                trees[trees.findIndex((value,index,obj) => {
+                    if(collide===value)
+                    {
+                        return true
+                    }
+                    return false;
+                })] = new Tree("Null",-1000,-1000);
+                collide.graphic.destroy();
+            }
         }else{
             collisionDirections.up = false;
             collisionDirections.down = false;
@@ -158,58 +203,35 @@ setInterval(() => {
             collisionDirections.right = false;
         }
 
+        if(score===required)
+        {
+            console.log("You're winner!");
+            trees.forEach((value,index,array) => {
+                value.graphic.destroy();
+            });
+            trees = [];
+            document.body.style.backgroundImage = `url("../Jackdaws and Kestrels/air.png")`;
+            document.body.style.backgroundRepeat = `no-repeat`;
+            document.body.style.backgroundSize = `100%`;
+        }
+
         if(collisionDirections.up)
         {
-            if(collisionDirections.left)
-            {
-                player.graphic.x = (collide.graphic.x + player.graphic.w) + 1;
-                player.graphic.y = (collide.graphic.y + player.graphic.h) + 1;
-            }
-            if(collisionDirections.right)
-            {
-                player.graphic.x = (collide.graphic.x - player.graphic.w) - 1;
-                player.graphic.y = (collide.graphic.y + player.graphic.h) + 1;
-            }
-                player.graphic.y = (collide.graphic.y + collide.graphic.h) + 1;
+                //if(Math.abs(player.graphic.y < (collide.graphic.y + collide.graphic.h/2)) && Math.abs(player.graphic.y < (collide.graphic.y + collide.graphic.h/2)))
+                //{
+                    player.graphic.y = (collide.graphic.y + collide.graphic.h) + 1;
+                //}
         }
         else if(collisionDirections.down)
         {
-            if(collisionDirections.left)
-            {
-                player.graphic.x = (collide.graphic.x + player.graphic.w) + 1;
-            }
-            if(collisionDirections.right)
-            {
-                player.graphic.x = (collide.graphic.x - player.graphic.w) - 1;
-            }
                 player.graphic.y = (collide.graphic.y - player.graphic.h) - 1;
         }
         else if(collisionDirections.left)
         {
-            if(collisionDirections.down)
-            {
-                player.graphic.x = (collide.graphic.x + player.graphic.w) + 1;
-                player.graphic.y = (collide.graphic.y - player.graphic.h) - 1;
-            }
-            else if(collisionDirections.up)
-            {
-                player.graphic.x = (collide.graphic.x + player.graphic.w) + 1;
-                player.graphic.y = (collide.graphic.y + player.graphic.h) + 1;
-            }
                 player.graphic.x = (collide.graphic.x + collide.graphic.w) + 1;
         }
         else if(collisionDirections.right)
         {
-            if(collisionDirections.down)
-            {
-                player.graphic.x = (collide.graphic.x - player.graphic.w) - 1;
-                player.graphic.y = (collide.graphic.y - player.graphic.h) - 1;
-            }
-            else if(collisionDirections.up)
-            {
-                player.graphic.x = (collide.graphic.x - player.graphic.w) - 1;
-                player.graphic.y = (collide.graphic.y + player.graphic.h) + 1;
-            }
                 player.graphic.x = (collide.graphic.x - player.graphic.w) - 1;
         }
       if (keys.up) {
